32f8fff460c884e685ce27bb31ea7a7359935f20
van-dat <114898434+van-dat@users.noreply.github.com>
2023-07-12 16:19:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:94d26d0bcbe1d80ca6831c98c725ebf386b0ac58c4cdf282b1e5ae79d2e035a8
-size 1509
+<?php
+session_start();
+ob_start();
+include "./model/config.php";
+include "./model/menu.php";
+include "./view/header.php";
+if (isset($_GET['act'])) {
+  switch ($_GET['act']) {
+    case 'index':
+      if (isset($_POST['search'])&& $_POST['search'] !="") {
+       $search = $_POST['search'];
+       $kq =search($search);
+      }else {
+        $kq = getallfilm();
+      }
+      include "./view/main.php";
+      break;
+    case 'category':
+      if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $kq = getfilmcate($id);
+        include "./view/main.php";
+      }
+      break;
+    case 'watch':
+      if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $kq = getonefilm($id);
+        $movie = getEpisode($id);
+        $allcmt = getallcmt();
+        include "./view/watch.php";
+      }
+      if (isset($_POST['id'])) {
+        $iduser = $_POST['id'];
+        $cmt = $_POST['comment'];
+        addcmt($iduser, $cmt);
+      }
+      break;
+    case 'view':
+      if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $kq = getonefilm($id);
+        $movie = getEpisode($id);
+        include "./view/view.php";
+        // var_dump($view);
+      }
+
+      break;
+
+    case 'logout':
+      unset($_SESSION['user']);
+      header('location: index.php');
+      break;
+    case '':
+
+      break;
+
+    default:
+      # code...
+      break;
+  }
+} else {
+  $kq = getallfilm();
+  include "./view/main.php";
+}
+include "./view/footer.php";
